3680c7ddd0182e7df7ba8266960353b607ec47e0
prjctimg <prjctimg@outlook.com>
2026-05-28 15:02:44 +0200

    first test run...please don't break
    Added some icons from icons.kitchen. For now I just use the p5 asterisk
    logo
    Added a loading page... I read the docs but haven't built it yet
    
    And a whole other stuff I forgot to mention

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,16 @@
+import { useState } from "react";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import LoadingPage from "../components/LoadingPage";
+
+SplashScreen.setOptions({ duration: 800, fade: true });
 
 export default function RootLayout() {
+  const [ready, setReady] = useState(false);
+
+  if (!ready) {
+    return <LoadingPage onFinish={() => setReady(true)} />;
+  }
+
   return <Stack />;
 }
